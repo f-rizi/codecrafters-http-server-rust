a25adc7f12576663dcfdb5e9ae80f91654a65d6d
Fatemeh <fatemeh@Mac-Book-Pro-M1-2020.local>
2024-11-28 18:29:26 -0800

    comp

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ use std::env;
 use std::fs;
 use std::fs::File;
 
+extern crate libflate;
+
+use std::io;
+use libflate::gzip::Decoder;
+
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+
+use base64;
+
+
 fn main() {    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     let pool = ThreadPool::new(4);
@@ -36,7 +48,7 @@ fn handle_request(mut stream: TcpStream) {
     let path = request.0.get("path").unwrap();
 
     if method.eq("GET") {
-        handle_get_request(path.to_string(), stream, request.0);
+        handle_get_request(path.to_string(), stream, request.0, request.1);
     }
     else if method.eq("POST") {
         handle_post_request(path.to_string(), stream , request.0, request.1);
@@ -69,12 +81,11 @@ fn handle_save_files(path: String, stream: &mut TcpStream,  body: Vec<u8>) {
     stream.write_all(b"HTTP/1.1 201 Created\r\n\r\n").unwrap();
 }
 
-fn handle_get_request(path: String, mut stream: TcpStream , request: HashMap<String, String> ) {
+fn handle_get_request(path: String, mut stream: TcpStream , request: HashMap<String, String>, body: Vec<u8> ) {
     if path.starts_with("/user-agent") {
         handle_user_agent_request(request, &mut stream);
     } else if path.starts_with("/echo/") {
-        handle_echo_request(path.to_string(), &mut stream);
-    
+        handle_echo_request(path.to_string(), &mut stream, request, body);
     } else if path.starts_with("/sleep") {
         handle_sleep_request(&mut stream);
     }
@@ -130,10 +141,46 @@ fn handle_user_agent_request(request: HashMap<String, String>, stream: &mut TcpS
     stream.write(response.as_bytes()).unwrap();
 }
 
-fn handle_echo_request(path: String, stream: &mut TcpStream) {
+fn handle_echo_request(path: String, stream: &mut TcpStream, request: HashMap<String, String>, body: Vec<u8>) {
     let temp = path.strip_prefix("/echo/");
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", temp.unwrap().len(), temp.unwrap());        
-    stream.write(response.as_bytes()).unwrap();
+
+    if request.contains_key("Accept-Encoding") {
+        let encoding = request.get("Accept-Encoding").unwrap();
+        let encodings = encoding.split(", ").collect::<Vec<&str>>();
+
+        if encodings.contains(&"gzip") {
+            println!("Gzip encoding is present!");
+            let temp_byte = temp.unwrap().as_bytes();
+
+            // let mut input = io::stdin();
+            // let mut decoder = Decoder::new(&mut input).unwrap();
+            // io::copy(&mut decoder, &mut io::stdout()).unwrap();
+ 
+            let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+            e.write_all(temp_byte).expect("Failed to write to encoder");
+            let compressed_bytes = e.finish().expect("Failed to finish compression");
+
+            // Convert the compressed bytes to a Base64 string
+            let as_string = base64::encode(compressed_bytes);
+
+            let response = 
+            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: {}\r\n\r\n{}", 
+            as_string.len(), 
+            as_string);   
+            stream.write(response.as_bytes()).unwrap();
+     
+        }
+        else {
+            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", temp.unwrap().len(), temp.unwrap());   
+            stream.write(response.as_bytes()).unwrap();
+     
+
+        }
+    }
+    else {
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", temp.unwrap().len(), temp.unwrap());        
+        stream.write(response.as_bytes()).unwrap();
+    }
 }
 
 fn get_request_lines(stream: &mut TcpStream) -> (HashMap<String, String>, Vec<u8>) {
@@ -178,42 +225,3 @@ fn get_request_lines(stream: &mut TcpStream) -> (HashMap<String, String>, Vec<u8
 
     return (request_parts, body);
 }
-
-/*
-fn create_request(stream: &mut TcpStream) ->  HashMap<String, String> {
-    let request_lines = get_request_lines(stream);
-
-    let mut body = Vec::new();
-    let mut content_length = 0;
-
-    let mut request_parts: HashMap<String, String> = HashMap::new();
-
-    for (pos, line) in request_lines.iter().enumerate(){
-        if pos == 0 {
-            let parts = line.trim().split_whitespace().collect::<Vec<&str>>();
-            request_parts.insert(String::from("method"), String::from(parts[0]));
-            request_parts.insert(String::from("path"), String::from(parts[1]));
-        }
-        else {
-            let parts = line.trim().split(": ").collect::<Vec<&str>>();
-
-            if parts.len() == 2 {
-                let key = String::from(parts[0]);
-                let value = String::from(parts[1]);
-                request_parts.insert(key, value);
-            }
-        }
-    }
-
-    if request_parts.contains_key("Content-Length") {
-        let content_length = request_parts.get("Content-Length").unwrap().to_string();
-        let temp = content_length.parse::<usize>().unwrap_or(0);
-        if temp > 0 {
-            let mut buffer = vec![0; temp];
-            buf_reader.read_exact(&mut buffer).unwrap();
-            body = buffer;
-        }
-    }
-
-    return request_parts;
-}*/
